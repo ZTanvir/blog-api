b344b6280401c289faa64e8a6c4b3bb0a4647ebb
zahirul <zahirulopel@gmail.com>
2025-11-10 17:16:39 +0600

    add error handaler so the error return as json not html page

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const errorHandler = require("./middleware/errorHandler");
 
 app.get("/", (req, res, next) => {
+  throw new Error("Invalid request");
+
   res.status(200).json({ message: "Server is running" });
 });
+
+app.use(errorHandler);
 
 module.exports = app;
